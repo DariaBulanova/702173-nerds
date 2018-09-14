8cebc77090ab6d69d4485cf065c3d058180221aa
DariaBulanova <42136192+DariaBulanova@users.noreply.github.com>
2018-09-14 13:59:34 +0300

    К макету4

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-<script>
-  var link = document.querySelector(".write");
-  var popup = document.querySelector(".modal-form");
-  var close = document.querySelector(".modal-close")
-  link.addEventListener("click", function(evt) {
+var link = document.querySelector(".write");
+var popup = document.querySelector(".modal-form");
+var close = document.querySelector(".modal-close")
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  var close = popup.querySelector(".modal-close");
+  close.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classList.add("modal-show");
-    var close = popup.querySelector(".modal-close");
-    close.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup.classList.remove("modal-show");
+    popup.classList.remove("modal-show");
 
-      window.addEventListener("keydown", function(evt) {
-        if (evt.keyCode === 27) {
-          evt.preventDefault();
-          if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-          }
+    window.addEventListener("keydown", function(evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+          popup.classList.remove("modal-show");
         }
-      });
+      }
     });
   });
-</script>
+});
